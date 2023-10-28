cb2e5b5ad4bd54a07670ed08c49336686177a062
BartoszBugla <48729791+BartoszBugla@users.noreply.github.com>
2023-10-28 12:25:56 +0200

    dsds

--- funcs.py
+++ funcs.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def rastrigin(x):
+def Rastrigin(x):
     return 10 * len(x) + sum([((xi) ** 2 - 10 * np.cos(2 * np.pi * (xi))) for xi in x])
 
 
@@ -9,7 +9,7 @@ def base(x):
     return x[0] * x[1]
 
 
-def rosenbrock(x):
+def Rosenbrock(x):
     n = len(x)
     result = 0
     for i in range(n - 1):
@@ -21,11 +21,11 @@ def xd(X):
     return (X[0] + 1) ** 2 - 1
 
 
-def sphere_function(x):
+def Sphere(x):
     return sum(xi**2 for xi in x)
 
 
-def beale_function(X):
+def Beale(X):
     x, y = X
     term1 = (1.5 - x + x * y) ** 2
     term2 = (2.25 - x + x * y**2) ** 2
@@ -33,7 +33,7 @@ def beale_function(X):
     return term1 + term2 + term3
 
 
-def bukin_function_n6(X):
+def Bukin(X):
     x, y = X
     term1 = 100 * abs(y - 0.01 * x**2) ** 0.5
     term2 = 0.01 * abs(x + 10)
