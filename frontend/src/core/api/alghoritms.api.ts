44437c4d2a075bd05019c7f3d30a4e25ded47912
BartoszBugla <48729791+BartoszBugla@users.noreply.github.com>
2023-12-27 19:57:39 +0100

    Fix build

--- frontend/src/core/api/alghoritms.api.ts
+++ frontend/src/core/api/alghoritms.api.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-import { ParamInfo, api } from '.';
+import { api } from '.';
 
 interface UseAlgorithmsApiOptions {
   enableGetAll?: boolean;
